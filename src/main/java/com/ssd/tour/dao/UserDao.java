b6cae1f7e48e841478853fa4ba3c0beff3b7b760
Sanjsigan <sanjsi0507@gmail.com>
2020-05-14 10:33:47 +0530

    start to modified UserDao

--- src/main/java/com/ssd/tour/dao/UserDao.java
+++ src/main/java/com/ssd/tour/dao/UserDao.java
@@ -1,5 +1,11 @@
 package com.ssd.tour.dao;
 
 public class UserDao {
+	
+	private static UserDao newInstance = new UserDao();
+	
+	public static UserDao getInstance() {
+		return newInstance;
+	}
 
 }
