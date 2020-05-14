c75e3bf068bfb05776ed24cdb78f43028ce2f8b2
Sanjsigan <sanjsi0507@gmail.com>
2020-05-14 10:27:29 +0530

    modified configs class

--- src/main/java/com/ssd/tour/dao/Configs.java
+++ src/main/java/com/ssd/tour/dao/Configs.java
@@ -1,5 +1,18 @@
 package com.ssd.tour.dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Configs {
+	
+public static Connection getDbConnection() throws ClassNotFoundException, SQLException {
+		
+		String connURL = "jdbc:mysql://localhost:3306/pvmstbooking";
+		
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(connURL, "root", "sanjsi");
+		return conn;
+	}
 
 }
