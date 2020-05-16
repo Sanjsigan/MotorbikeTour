2ed856f082208e5d0123158b678a693dd10d0fd4
Sanjsigan <sanjsi0507@gmail.com>
2020-05-16 11:07:03 +0530

    craete some classes users

--- src/main/java/com/ssd/tour/UserResource.java
+++ src/main/java/com/ssd/tour/UserResource.java
@@ -1,7 +1,11 @@
 package com.ssd.tour;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
-
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
@@ -20,7 +24,79 @@ import com.ssd.tour.model.Users;
 
 public class UserResource {
 	
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAll() {
+		//Jackson
+		//GSON
+		Object object = UserDao.getInstance().getAll();
 		
+		Gson gson = new Gson();
+		
+		if(object instanceof List<?>) { //Data type  checking.
+			
+			@SuppressWarnings("unchecked")
+			List<Users> cars = (List<Users>)object; //Casting.
+			
+			String jsonString = gson.toJson(cars);
+			return Response
+					.status(200)
+					.entity(jsonString)
+					.build();
+		} else {
+			@SuppressWarnings("unchecked")
+			Map<String, String> errMsg = (Map<String, String>)object; //Casting.
+			
+			String jsonString = gson.toJson(errMsg);
+			return Response
+					.status(200)
+					.entity(jsonString)
+					.build();
+		}
+		
+	}
+	
+	@GET
+	@Path("{id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getAUser(@PathParam("id") String id) {
+		
+		int carid = Integer.parseInt(id);
+		
+		Users user = UserDao.getInstance().get(carid);
+		
+		Gson gson = new Gson();
+		String jsonString = gson.toJson(user);
+		
+		return Response
+				.status(200)
+				.entity(jsonString)
+				.build();
+		
+	}
+	
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public void adduser(@FormParam("user_id") int id,
+							@FormParam("user_name") String name,
+							@FormParam("user_address") String address,
+							@FormParam("user_mobile") int mobile,
+							@FormParam("user_email") String email,
+							@FormParam("password") String password)
+									throws ClassNotFoundException, SQLException {
+		
+		Users user= new Users();
+		
+		user.setId(id);
+		user.setName(name);
+		user.setAddress(address);
+		user.setMobile(mobile);
+		user.setMail(email);
+		user.setMail(password);
+		
+		//boolean result = CarDao.getInstance().add(car);
+	
+	}
 
 	
 }
--- src/main/java/com/ssd/tour/dao/UserDao.java
+++ src/main/java/com/ssd/tour/dao/UserDao.java
@@ -13,7 +13,124 @@ import com.ssd.tour.model.Users;
 
 public class UserDao {
 
+	private static UserDao newInstance = new UserDao();
+
+	public static UserDao getInstance() {
+		return newInstance;
+	}
+
+	private UserDao() {
+
+	}
 	
+	//Fetching data from database.
+		public Object getAll(){
+			List<Users> carList = new ArrayList<Users>();
+			
+			
+			try {
+				
+				Connection conn = Configs.getDbConnection();
+				
+				String sql = "SELECT * FROM signup";//Query to be execute
+				PreparedStatement preparedStatement = conn.prepareStatement(sql);
+				
+				ResultSet resultSet = preparedStatement.executeQuery(); //Query execution.
+				
+				while(resultSet.next()) {
+					Users user = new Users();
+					
+					user.setId(resultSet.getInt("user_id"));
+					user.setName(resultSet.getString("user_name"));
+					user.setAddress(resultSet.getString("user_address"));
+					user.setMobile(resultSet.getInt("user_mobile"));
+					user.setMail(resultSet.getString("user_email"));
+					user.setPasword(resultSet.getString("password"));
+					
+					
+					carList.add(user);
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Error : "+e.getMessage());
+				
+				Map<String, String> errMsg = new HashMap<String, String>();
+				errMsg.put("Error",  e.getMessage());
+				
+				return errMsg;
+				
+			}
+			
+			return carList;
+			
+		}
+		
+		//Fetching a single data from database.
+		public Users get(int user_id){
+			Users user = new Users();
+			
+			
+			try {
+				
+				Connection conn = Configs.getDbConnection();
+				
+				String sql = "SELECT * FROM user WHERE user_id = ?";//Query to be execute
+				PreparedStatement preparedStatement = conn.prepareStatement(sql);
+				preparedStatement.setInt(1, user_id); //binding the parameter value, 1 is for specify first parameter.
+				
+				ResultSet resultSet = preparedStatement.executeQuery(); //Query execution.
+				
+				while(resultSet.next()) {
+					
+					user.setId(resultSet.getInt("user_id"));
+					user.setName(resultSet.getString("user_name"));
+					user.setAddress(resultSet.getString("user_address"));
+					user.setMobile(resultSet.getInt("user_mobile"));
+					user.setMail(resultSet.getString("user_email"));
+					user.setPasword(resultSet.getString("password"));
+					
+					
+				}
+				
+			} catch (Exception e) {
+				e.printStackTrace();
+				System.out.println("Error : "+e.getMessage());
+			}
+			
+			return user;
+			
+		}
+		
+		public boolean add(Users user) throws ClassNotFoundException, SQLException {
+			
+			if(user != null) {
+				
+				Connection connection = Configs.getDbConnection();
+				String sql = "INSERT INTO `signup` (`user_id`,`user_name`, `user_address`, `user_mobile`, `user_email`,`password`) " + 
+							"VALUES (?,?,?,?,?,?)";
+				PreparedStatement stmt = connection.prepareStatement(sql);
+				stmt.setInt(1, user.getId());
+				stmt.setString(2, user.getName());
+				stmt.setString(3, user.getAddress());
+				stmt.setInt(4, user.getMobile());
+				stmt.setString(5, user.getMail());
+				stmt.setString(6, user.getPassword());
+				
+				int count = stmt.executeUpdate();
+				
+				if(count > 0) {
+					System.out.println("Successfull Added");
+					return true;
+				} else {
+					System.out.println("Error occured");
+				}
+								
+			}
+			
+			return false;
+			
+		}
 
 
 }
