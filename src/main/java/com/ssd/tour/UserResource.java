1a0a7ac77a6246f72928dec0c4f4a49e00acdc01
pethurusan <antonypethurusan@gmail.com>
2020-05-20 17:58:44 +0530

    Modify HotelResource.java class

--- src/main/java/com/ssd/tour/UserResource.java
+++ src/main/java/com/ssd/tour/UserResource.java
@@ -1,11 +1,6 @@
 package com.ssd.tour;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 
