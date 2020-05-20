0c1a9f8eefc4ee4e366483672a61e51a1b13254c
pethurusan <antonypethurusan@gmail.com>
2020-05-20 17:49:34 +0530

    Modify and change format UserResource.java class

--- src/main/java/com/ssd/tour/UserResource.java
+++ src/main/java/com/ssd/tour/UserResource.java
@@ -23,80 +23,67 @@ import com.ssd.tour.dao.UserDao;
 import com.ssd.tour.model.Users;
 
 public class UserResource {
-	
+
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAll() {
-		//Jackson
-		//GSON
+		// Jackson
+		// GSON
 		Object object = UserDao.getInstance().getAll();
-		
+
 		Gson gson = new Gson();
-		
-		if(object instanceof List<?>) { //Data type  checking.
-			
+
+		if (object instanceof List<?>) { // Data type checking.
+
 			@SuppressWarnings("unchecked")
-			List<Users> cars = (List<Users>)object; //Casting.
-			
+			List<Users> cars = (List<Users>) object; // Casting.
+
 			String jsonString = gson.toJson(cars);
-			return Response
-					.status(200)
-					.entity(jsonString)
-					.build();
+			return Response.status(200).entity(jsonString).build();
 		} else {
 			@SuppressWarnings("unchecked")
-			Map<String, String> errMsg = (Map<String, String>)object; //Casting.
-			
+			Map<String, String> errMsg = (Map<String, String>) object; // Casting.
+
 			String jsonString = gson.toJson(errMsg);
-			return Response
-					.status(200)
-					.entity(jsonString)
-					.build();
+			return Response.status(200).entity(jsonString).build();
 		}
-		
+
 	}
-	
+
 	@GET
 	@Path("{id}")
 	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAUser(@PathParam("id") String id) {
-		
+
 		int carid = Integer.parseInt(id);
-		
+
 		Users user = UserDao.getInstance().get(carid);
-		
+
 		Gson gson = new Gson();
 		String jsonString = gson.toJson(user);
-		
-		return Response
-				.status(200)
-				.entity(jsonString)
-				.build();
-		
+
+		return Response.status(200).entity(jsonString).build();
+
 	}
-	
+
 	@POST
 	@Produces(MediaType.APPLICATION_JSON)
-	public void adduser(@FormParam("user_id") int id,
-							@FormParam("user_name") String name,
-							@FormParam("user_address") String address,
-							@FormParam("user_mobile") int mobile,
-							@FormParam("user_email") String email,
-							@FormParam("password") String password)
-									throws ClassNotFoundException, SQLException {
-		
-		Users user= new Users();
-		
+	public void adduser(@FormParam("user_id") int id, @FormParam("user_name") String name,
+			@FormParam("user_address") String address, @FormParam("user_mobile") int mobile,
+			@FormParam("user_email") String email, @FormParam("password") String password)
+			throws ClassNotFoundException, SQLException {
+
+		Users user = new Users();
+
 		user.setId(id);
 		user.setName(name);
 		user.setAddress(address);
 		user.setMobile(mobile);
 		user.setMail(email);
 		user.setMail(password);
-		
-		//boolean result = CarDao.getInstance().add(car);
-	
+
+		// boolean result = CarDao.getInstance().add(car);
+
 	}
 
-	
 }
