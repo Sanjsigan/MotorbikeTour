3efcd2e2dd51f070d268afe6ed1778d6e6968c75
pethurusan <antonypethurusan@gmail.com>
2020-05-20 15:08:44 +0530

    Generate getter and setter in Hotel.java

--- src/main/java/com/ssd/tour/model/Hotel.java
+++ src/main/java/com/ssd/tour/model/Hotel.java
@@ -1,5 +1,34 @@
 package com.ssd.tour.model;
 
 public class Hotel {
-
+	private int ID;
+	private String Name;
+	private String Address;
+	private double Amount;
+	
+	public int getID() {
+		return ID;
+	}
+	public void setID(int iD) {
+		ID = iD;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public double getAmount() {
+		return Amount;
+	}
+	public void setAmount(double amount) {
+		Amount = amount;
+	}
+	
 }
