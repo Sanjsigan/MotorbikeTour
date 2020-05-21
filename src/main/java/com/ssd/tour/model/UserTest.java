afb82b3034c71d82f2c1e0e6d79cbeba931ba152
Vinthusha <vinthushalogan@gmail.com>
2020-05-21 11:40:38 +0530

    done to create class UserTest.java class

--- src/main/java/com/ssd/tour/model/UserTest.java
+++ src/main/java/com/ssd/tour/model/UserTest.java
@@ -1,7 +1,29 @@
 package com.ssd.tour.model;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
 public class UserTest {
 	
+	@SuppressWarnings("deprecation")
+	@Test
 	
+	public void TestUser() {
+		int id= 987;
+		String name="user1";
+		String address="adderess123";
+		int mobile =0765432134;
+		String password ="sdf@#$5ss";
+		
+		
 
+         Users user = new Users(id,name,address,mobile,password, password);
+		
+	     Assert.assertTrue(user.getAddress().equals(address));
+	     Assert.assertTrue(user.getPassword().equals(password));
+	     
+	
+	
+	}
 }
