5c3805ff2c065be6aa8fc96e2a13211ddaab87d2
Vinthusha <vinthushalogan@gmail.com>
2020-05-21 11:24:02 +0530

    Done To create BookingTest.java clas

--- src/main/java/com/ssd/tour/model/BookingTest.java
+++ src/main/java/com/ssd/tour/model/BookingTest.java
@@ -1,5 +1,24 @@
 package com.ssd.tour.model;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+@SuppressWarnings("deprecation")
 public class BookingTest {
+
+	@Test
+
+	public void TestBooking() {
+		
+		int id =230;
+	    String name ="user1";
+	    String mail ="user123@gmail.com";
+	    
+	    Booking booking = new Booking(id,name,mail,id,id);
+	    
+	    Assert.assertTrue(booking.getMail().equals(mail));
+	   Assert.assertTrue(booking.getName().equals(name));
+	}
 
 }
