5daa5f7fd48e3b0dbbfbf671d39e697d25457d68
Vinthusha <vinthushalogan@gmail.com>
2020-05-21 11:04:11 +0530

    Done to create Junit test class BikeTest.java

--- src/main/java/com/ssd/tour/model/TestBike.java
+++ src/main/java/com/ssd/tour/model/TestBike.java
@@ -1,5 +1,22 @@
 package com.ssd.tour.model;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+@SuppressWarnings("deprecation")
 public class TestBike {
+
+	@Test
+	public void BikeTest() {
+		int id = 100;
+		String model = "model1";
+		String Horse = "Horsea122";
+
+		Bike bike = new Bike(id, model, Horse, id);
+
+		Assert.assertTrue(bike.getModel().equals(model));
+
+	}
 
 }
