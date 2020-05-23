cc615f5a90f999a57b0082a656419aa9e1adda45
Vinthusha <vinthushalogan@gmail.com>
2020-05-23 21:36:13 +0530

    done to create TestHotel.java class

--- src/main/java/com/ssd/tour/model/TestHotel.java
+++ src/main/java/com/ssd/tour/model/TestHotel.java
@@ -1,5 +1,24 @@
 package com.ssd.tour.model;
 
+import org.junit.Test;
+
+import junit.framework.Assert;
+
+@SuppressWarnings("deprecation")
 public class TestHotel {
+	
+	
+	@Test
+	
+	public void HotelTest() {
+		int id=232;
+		String name="hetel3434";
+		String address ="address!@#$~";
+		
+		Hotel hotel = new Hotel(id, name, address, id);
+		
+		Assert.assertTrue( hotel.getName().equals(name));
+		Assert.assertTrue( hotel.getAddress().equals(address));
+	}
 
 }
