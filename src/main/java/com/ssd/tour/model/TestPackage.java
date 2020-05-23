6d08452f0185ee96cb9ee1c861395fd53eb7d183
Vinthusha <vinthushalogan@gmail.com>
2020-05-23 21:21:26 +0530

    declare variables

--- src/main/java/com/ssd/tour/model/TestPackage.java
+++ src/main/java/com/ssd/tour/model/TestPackage.java
@@ -1,7 +1,22 @@
 package com.ssd.tour.model;
 
+import org.junit.Test;
+
 public class TestPackage {
 	
+	@Test
 	
+	public void PackageTest() {
+		
+	int id=987;
+	String name="pac34";
+	String type ="type34";
+	int amount=87766;
+	String desc ="desc@#";
+	
+	
+	
+		
+	}
 
 }
