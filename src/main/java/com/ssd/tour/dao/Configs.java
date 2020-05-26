69592b30f4f400879b92dad3bdac5c5da7eadaaa
Sanjsigan <sanjsi0507@gmail.com>
2020-05-26 22:53:49 +0530

    change Heroku URL In config class

--- src/main/java/com/ssd/tour/dao/Configs.java
+++ src/main/java/com/ssd/tour/dao/Configs.java
@@ -8,7 +8,7 @@ public class Configs {
 	
 public static Connection getDbConnection() throws ClassNotFoundException, SQLException {
 		
-		String connURL = "jdbc:mysql://localhost:3306/pvmstbooking";
+		String connURL = "jdbc:mysql://us-cdbr-east-05.cleardb.net/heroku_7a36e184aea2372";
 		
 		Class.forName("com.mysql.cj.jdbc.Driver");
 		Connection conn = DriverManager.getConnection(connURL, "b27bd715e3b7e6", "28e05a89");
